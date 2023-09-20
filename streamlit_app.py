7e84442da01532a24b262a249a692877f6cc9970
Dennis Wessel <dwessel@emoneyadvisor.com>
2023-09-20 14:03:23 -0400

    Initial Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.write('Hello World')
+st.write("""
+         #Your Free Dream Interpreter
+         
+         Describe your dream!
+
+         Tip 1: Include any feelings or thoughts you had in the dream
+           Ex: I saw my Grandfather sitting at a bar. I was excited to see him and he was pleased to see me but not excited.
+           Ex: An eagle flew up to me. He was there to rescue me.
+         
+         """) 
